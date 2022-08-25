58dd5d3db40013dbdd69a52f308b8e8b69af462a
Luther Tchofo-Safo <nintendobserver@gmail.com>
2022-08-25 16:07:14 +0200

    A lot but especially username at sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true
+
   has_many :games
   has_many :bookings
 
