0e23db5b6a432f4f84384123fe28046bfd38fb0d
Luther Tchofo-Safo <nintendobserver@gmail.com>
2022-08-26 14:38:41 +0200

    Removed extra enum statuses.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :game
-  enum status: %i[opened pending ongoing returning closed canceled]
+  enum status: %i[pending ongoing closed canceled]
+  # enum status: %i[opened pending ongoing returning closed canceled]
 
   def color_class
     case status
